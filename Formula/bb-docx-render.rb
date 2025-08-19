8c2e40598ea8262c9c8af37a32581a1396813537
Ringo@Mac <binhthanhgo@gmail.com>
2025-08-19 12:57:41 +0700

    change uv dependencies in homebrew

--- Formula/bb-docx-render.rb
+++ Formula/bb-docx-render.rb
@@ -1,6 +1,4 @@
 class BbDocxRender < Formula
-  include Language::Python::Virtualenv
-
   desc "Render DOCX templates using Babashka and Python"
   homepage "https://github.com/DrRingo/bb-docx-render"
   url "https://github.com/DrRingo/bb-docx-render/archive/refs/tags/0.1.tar.gz"
@@ -8,28 +6,19 @@ class BbDocxRender < Formula
   license "Apache-2.0"
 
   depends_on "babashka"
-  depends_on "python@3.11"
-
-  resource "docxtpl" do
-    url "https://files.pythonhosted.org/packages/8c/3a/de0754ed1bcc47210307b7a98708d71207eb3f52594e2cf23a1ae97d7f08/docxtpl-0.20.1.tar.gz"
-    sha256 "5bab05a2c60b225c730ce18e9e2d40d748b4a12f599e150badbcb659088a51d4"
-  end
-
-  resource "jinja2" do
-    url "https://files.pythonhosted.org/packages/df/bf/f7da0350254c0ed7c72f3e33cef02e048281fec7ecec5f032d4aac52226b/jinja2-3.1.6.tar.gz"
-    sha256 "0137fb05990d35f1275a587e9aee6d56da821fc83491a0fb838183be43f66d6d"
-  end
-
-  resource "python-docx" do
-    url "https://files.pythonhosted.org/packages/a9/f7/eddfe33871520adab45aaa1a71f0402a2252050c14c7e3009446c8f4701c/python_docx-1.2.0.tar.gz"
-    sha256 "7bc9d7b7d8a69c9c02ca09216118c86552704edc23bac179283f2e38f86220ce"
-  end
+  depends_on "uv"
 
   def install
-    virtualenv_install_with_resources
-    libexec.install "fill_docx.bb"
-    (bin/"fill-docx").write_env_script libexec/"fill_docx.bb",
-      PATH: "#{libexec}/bin:$PATH"
+    # The script uses `uv` to manage its own Python environment based on pyproject.toml,
+    # so we don't need to create a virtualenv with resources manually.
+    # We just need to install the script and its config, then create a wrapper.
+    libexec.install "fill_docx.bb", "pyproject.toml"
+    (bin/"fill-docx").write_script <<~EOS
+      #!/bin/bash
+      # Change to the script's directory so `uv` can find pyproject.toml
+      cd "#{libexec}"
+      exec "#{Formula["babashka"].opt_bin}/bb" "fill_docx.bb" "$@"
+    EOS
   end
 
   test do
