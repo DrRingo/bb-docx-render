42e563284df8cd995e86655809164e1ebe629763
Ringo@Mac <binhthanhgo@gmail.com>
2026-04-05 15:49:22 +0700

    fix(brew): add exact sha256 checksums for 1.0.0 binaries

--- Formula/bb-docx-render.rb
+++ Formula/bb-docx-render.rb
@@ -7,17 +7,17 @@ class BbDocxRender < Formula
   on_macos do
     on_arm do
       url "https://github.com/DrRingo/bb-docx-render/releases/download/v1.0.0/fill-docx-macos"
-      sha256 :no_check
+      sha256 "0b80d21967a899f17d48fbc85081d86752a1d0a12fc0986d4c64709f828d9720"
     end
     on_intel do
       url "https://github.com/DrRingo/bb-docx-render/releases/download/v1.0.0/fill-docx-macos"
-      sha256 :no_check
+      sha256 "0b80d21967a899f17d48fbc85081d86752a1d0a12fc0986d4c64709f828d9720"
     end
   end
 
   on_linux do
     url "https://github.com/DrRingo/bb-docx-render/releases/download/v1.0.0/fill-docx-linux"
-    sha256 :no_check
+    sha256 "d14adaebcef494a5e18652f1aa36038524313bbef1d574f5de84d0146f4bc85c"
   end
 
   def install
